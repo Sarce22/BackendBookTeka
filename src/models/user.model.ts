abc3946cf1105e1e0137a4da4d1a276c193133bf
Sebastian Arce <sebastianarce482@gmail.com>
2023-10-28 16:52:07 -0500

    Hola

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
     email: { type: String, required: true, unique:true },
     password: { type: String, required: true },
     role: { type: String, required: true }
+    
 }, {
     timestamps: true,
     versionKey: false
